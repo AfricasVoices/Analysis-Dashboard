26050c990d6424a0bdeb9e28ecd8ccfddc886ff1
Alexander Simpson <as2388@cam.ac.uk>
2023-05-12 15:21:52 +0100

    Move Firestore setup into a beforeAll()

--- tests/firebase-emulator-connection-tests/firestore-emulator-connection.test.ts
+++ tests/firebase-emulator-connection-tests/firestore-emulator-connection.test.ts
@@ -1,34 +1,37 @@
 import { assertSucceeds, initializeTestEnvironment } from "@firebase/rules-unit-testing";
-import { doc, getDoc, setDoc } from "firebase/firestore"
-import { describe, expect, test } from "vitest"
+import { doc, getDoc, setDoc, DocumentReference } from "firebase/firestore"
+import { describe, expect, test, beforeAll } from "vitest"
 
-describe("Test the connection to the Firestore emulator is working", async () => {
-    let testEnv = await initializeTestEnvironment({
-        projectId: "avf-analysis-dashboard",
-        firestore: {
-            // Allow global access to everything for now, so we can initialise the database
-            rules:
-                "service cloud.firestore { match /databases/{database}/documents { match /{document=**} { " +
-                "allow read, write: if true;" +
-                "}}}"
-        }
+describe("Test the connection to the Firestore emulator is working", () => {
+    let ref: DocumentReference;
+    beforeAll(async () => {
+        let testEnv = await initializeTestEnvironment({
+            projectId: "avf-analysis-dashboard",
+            firestore: {
+                // Allow global access to everything for now, so we can initialise the database
+                rules:
+                    "service cloud.firestore { match /databases/{database}/documents { match /{document=**} { " +
+                    "allow read, write: if true;" +
+                    "}}}"
+            }
+        });
+
+        // Reset the emulator in case it was used for any previous tests
+        await testEnv.clearFirestore();
+
+        // We can use an unauthenticated context for admin at this point because we haven't set
+        // any rules yet.
+        const admin = testEnv.unauthenticatedContext();
+        ref = doc(admin.firestore(), "users/test");
     })
-
-    // Reset the emulator in case it was used for any previous tests
-    await testEnv.clearFirestore();
 
     const docToWrite = {
         "test": "test"
     }
 
-    // We can use an unauthenticated context for admin at this point because we haven't set
-    // any rules yet.
-    const admin = testEnv.unauthenticatedContext();
-    const ref = doc(admin.firestore(), "users/test")
-
     // Check the Firestore emulator connection is working
     test("Can write a test document to the Firestore emulator", async () => {
-        await assertSucceeds(setDoc(ref, docToWrite))
+        await assertSucceeds(setDoc(ref, docToWrite));
     });
 
     test("Can read the test document from the Firestore emulator", async () => {
