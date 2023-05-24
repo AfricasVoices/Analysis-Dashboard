48d8abaae520ac306c028a2fe9bc17a0c9379976
Alexander Simpson <as2388@cam.ac.uk>
2023-05-24 11:47:31 +0100

    Include tests/ in format rules

--- tests/firebase-emulator-connection-tests/firestore-emulator-connection.test.ts
+++ tests/firebase-emulator-connection-tests/firestore-emulator-connection.test.ts
@@ -1,6 +1,9 @@
-import { assertSucceeds, initializeTestEnvironment } from "@firebase/rules-unit-testing";
-import { doc, getDoc, setDoc, DocumentReference } from "firebase/firestore"
-import { describe, expect, test, beforeAll } from "vitest"
+import {
+    assertSucceeds,
+    initializeTestEnvironment,
+} from "@firebase/rules-unit-testing";
+import { doc, getDoc, setDoc, DocumentReference } from "firebase/firestore";
+import { describe, expect, test, beforeAll } from "vitest";
 
 describe("Test the connection to the Firestore emulator is working", () => {
     let ref: DocumentReference;
@@ -12,8 +15,8 @@ describe("Test the connection to the Firestore emulator is working", () => {
                 rules:
                     "service cloud.firestore { match /databases/{database}/documents { match /{document=**} { " +
                     "allow read, write: if true;" +
-                    "}}}"
-            }
+                    "}}}",
+            },
         });
 
         // Reset the emulator in case it was used for any previous tests
@@ -23,18 +26,18 @@ describe("Test the connection to the Firestore emulator is working", () => {
         // any rules yet.
         const admin = testEnv.unauthenticatedContext();
         ref = doc(admin.firestore(), "users/test");
-    })
+    });
 
     const docToWrite = {
-        "test": "test"
-    }
+        test: "test",
+    };
 
     test("Can write a test document to the Firestore emulator", async () => {
         await assertSucceeds(setDoc(ref, docToWrite));
     });
 
     test("Can read the test document from the Firestore emulator", async () => {
-        const testDoc = await getDoc(ref)
+        const testDoc = await getDoc(ref);
         expect(testDoc.data()).toStrictEqual(docToWrite);
     });
-})
+});
